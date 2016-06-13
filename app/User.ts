ee0abc4dac78f58e41084307dafa34e3acb5f97c
Amit Thakkar <vigildbest@gmail.com>
2016-06-13 12:07:38 +0530

    Adding demo for ?.

---
+++ app/User.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by Amit Thakkar on 6/13/16.
+ */
+export interface User {
+    name:string
+    age:number
+}
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,29 @@
  * Created by Amit Thakkar on 6/13/16.
  */
 import {Component} from '@angular/core';
+import {User} from './user'
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+        <h1>Old Way(in Angular1.x)</h1>
+        <div>
+            User Details:
+            Name: {{user && user.name}}
+            Age: {{user && user.age}}
+        </div>        
+        <hr />
+        <h1>New Way(in Angular2 - Safe Navigation Operator)</h1>
+        <div>
+            User Details:
+            Name: {{user?.name}}
+            Age: {{user?.age}}
+        </div>        
+    `
 })
 export class AppComponent {
+    user:User = <User>{
+        name: 'Amit Thakkar',
+        age: 27
+    };
 }
